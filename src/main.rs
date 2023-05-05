c4f8ee3ac541a25c794abd050775d1440d16c13e
maximo santoro <maximo.santoro@iovlabs.org>
2023-05-05 20:31:12 -0300

    add intro and lines, and journal dir

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use std::fs::File;
 use std::io::prelude::*;
 use chrono;
+use std::fs;
 
 fn main() {
     let local_time = chrono::offset::Local::now();
     let time_str = local_time.format("%Y-%m-%d_%H%M").to_string();
-    // println!("{}", time_str);
 
+    fs::create_dir_all("journal")
+        .expect("error creating directory");
 
     //let year = time_str.substring(0, 3);
    // let month = time_str.substring(5, 7);
-    // let month
     let file_name = format!("journal/{}.txt", time_str);
-    // let full_directory /YYYY/MM/YYYY-MM-DD-HHMM.txt
+    let file_print = format!("today: {}\n\n\n", time_str);
     let mut file = File::create(&file_name).
         expect("error while creating file");
-    file.write_all(time_str.as_bytes()).expect("Unable to write data");
+    file.write_all(file_print.as_bytes()).expect("Unable to write data");
+    println!("{}", file_name);
+
 }
