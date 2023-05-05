bf89756d09b8d49a320097e61d54499f32a1b72e
maximo santoro <maximo.santoro@iovlabs.org>
2023-05-04 22:14:50 -0300

    add journal and .gitignore

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ use chrono;
 
 fn main() {
     let local_time = chrono::offset::Local::now();
-    let time_str = local_time.format("%Y-%m-%d-%H%M").to_string();
-//    println!("{}", time_str);
-    let file_name = format!("{}.txt", time_str);
+    let time_str = local_time.format("%Y-%m-%d_%H%M").to_string();
+    // println!("{}", time_str);
+
+
+    //let year = time_str.substring(0, 3);
+   // let month = time_str.substring(5, 7);
+    // let month
+    let file_name = format!("journal/{}.txt", time_str);
+    // let full_directory /YYYY/MM/YYYY-MM-DD-HHMM.txt
     let mut file = File::create(&file_name).
         expect("error while creating file");
     file.write_all(time_str.as_bytes()).expect("Unable to write data");
