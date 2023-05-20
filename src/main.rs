57d74a777cd2c53239b269328efc40dabe0cc90b
maximo santoro <maximo.santoro@iovlabs.org>
2023-05-20 20:05:03 -0300

    adds

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::fs::File;
 use std::io::prelude::*;
 use chrono;
 use std::fs;
+use std::path::Path;
+use std::fs::OpenOptions;
+
 
 fn main() {
 
@@ -9,16 +12,31 @@ fn main() {
     // la idea seria armar un archivo de texto de cada mes
     // usar una db - indexar años, mess 
     //      ek
+    // YYYY-MM-DD-HHMM
     let local_time = chrono::offset::Local::now();
     let time_str = local_time.format("%Y-%m-%d-%H%M").to_string();
     fs::create_dir_all("journal")
         .expect("error creating directory");
+    let file_name = &time_str[0..10];
+    let hour_min = &time_str[11..14];
+    println!("file_name: {}, hour_min: {}", file_name, hour_min);
 
-    let file_name = format!("journal/{}.txt", time_str);
+    let directory = format!("journal/{}.txt", file_name);
     let file_print = format!("today: {}\n\n\n", time_str);
-    let mut file = File::create(&file_name).
-        expect("error while creating file");
-    file.write_all(file_print.as_bytes()).expect("Unable to write data");
-    println!("{}", file_name);
+    
+    let path = Path::new(file_name);
+    
+    if path.exists() {
+        let file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(directory);
+        writeln!(file.expect("XD"), "\n\n{}\n\n\n", hour_min);
+    } else {
+        let file = File::create(&directory).
+            expect("error while creating file");
+        file.write_all(file_print.as_bytes()).expect("Unable to write data");
+    }
+    println!("{}", directory);
     
 }
