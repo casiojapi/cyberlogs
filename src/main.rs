702e309b060324d948e0f2c96b46c0c66244da1e
maximo santoro <maximo.santoro@iovlabs.org>
2023-05-23 12:03:54 -0300

    fixing fixing fixing dirs and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
 use std::io::prelude::*;
 use chrono;
-use std::fs;
 use std::path::Path;
+use std::env;
 use std::fs::OpenOptions;
 
 
@@ -15,14 +15,22 @@ fn main() {
     // YYYY-MM-DD-HHMM
     let local_time = chrono::offset::Local::now();
     let time_str = local_time.format("%Y-%m-%d-%H%M").to_string();
-    fs::create_dir_all("journal")
-        .expect("error creating directory");
+  //  fs::create_dir_all("journal")
+   //     .expect("error creating directory");
+    let root_dir = env::current_dir().expect("failed to retrieve current directory");
+    let journal_dir = root_dir.join("journal");
+
+    if let Err(_err) = std::fs::create_dir_all(&journal_dir) {
+        //eprintln!("Failed to create directory: {}", err);
+    } else {
+       // println!("Directory created successfully: {:?}", journal_dir);
+    }
     let file_name = &time_str[0..10];
     let hour_min = &time_str[11..15];
 //    println!("file_name: {}, hour_min: {}", file_name, hour_min);
 
-    let directory = format!("journal/{}.txt", file_name);
-    let file_print = format!("today: {}\n\n\n", time_str);
+    let directory = format!("{}/{}.txt",journal_dir.display(), file_name);
+    let file_print = format!("today: {}\n----------------------\n\n\n", time_str);
     
     let path = Path::new(&directory);
     
@@ -31,7 +39,8 @@ fn main() {
             .write(true)
             .append(true)
             .open(&directory);
-        writeln!(file.expect("XD"), "\n\nnew log entry at: {}\n\n\n", hour_min);
+        let file_append = format!("\n\nnew log entry at: {}\n----------------------\n\n\n", hour_min);
+        file.expect("Error with file").write_all(file_append.as_bytes()).expect("Unable to append data");
     } else {
         let mut file = File::create(&directory).
             expect("error while creating file");
